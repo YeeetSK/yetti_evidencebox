d446556abbf71e762c371e71b3bd3df193279429
CFOUR <116594943+cfoour@users.noreply.github.com>
2024-04-27 18:39:26 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,18 +2,13 @@ fx_version "cerulean"
 game "gta5"
 lua54 'yes'
 
+version '1.0.0'
+
 author 'Yetti Development'
 description 'Evidence boxes for police.'
 
-shared_scripts {
-    "config.lua",
-    '@ox_lib/init.lua',
-}
+shared_scripts {'@ox_lib/init.lua', 'config.lua'}
 
-client_scripts {
-    "client/client.lua",
-}
+client_scripts {'client/client.lua'}
 
-server_scripts {
-    'server/server.lua'
-}
+server_scripts {'server/server.lua'}
